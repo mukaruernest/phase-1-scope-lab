5cde01efacf958c8e9fb3e8c5427a5f7cd8a5b41
mukaruernest <mukaruernesto@gmail.com>
2023-01-13 10:35:33 +0300

    scope lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+// 
+
+// const upperCaseCustomerName = (name) => name.toUpperCase()
+// upperCaseCustomerName(customerName)
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Not Me'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'it is you!'
+}
